cdcbf3496d09017fafc27548eaa7274dcc2f0520
Playground <christopher@playground.tv>
2021-08-09 17:54:13 +0200

    Initial Commit

---
+++ Chat4App/Chat4AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat4AppApp.swift
+//  Chat4App
+//
+//  Created by Christopher on 2021-08-09.
+//
+
+import SwiftUI
+
+@main
+struct Chat4AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat4App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chat4App
+//
+//  Created by Christopher on 2021-08-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
